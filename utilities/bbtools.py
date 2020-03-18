3ff8f20cb5f053c5849eaa8e04a4a049155dea52
Jeff Kimbrel <jakpot@gmail.com>
2020-03-18 14:58:26 -0700

    utilities package

---
+++ utilities/bbtools.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+def bbduk_stats_parser(stats_file_path):
+    stats = pd.read_csv(stats_file_path, sep=None, names=[
+                        'TYPE', 'READS', 'PERCENT'], header=None, engine='python')
+    stats = stats.set_index('TYPE')
+
+    total = stats.loc['#Total', 'READS']
+    matched = stats.loc['#Matched', 'READS']
+
+    return(total, matched)
