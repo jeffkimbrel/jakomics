8cf736b522dcc4673b2a9ca7514958958a981ec2
Jeff Kimbrel <jakpot@gmail.com>
2020-04-07 10:15:46 -0700

    Create colors.py

---
+++ utilities/colors.py
@@ -0,0 +1,12 @@
+# https://stackoverflow.com/questions/287871/how-to-print-colored-text-in-terminal-in-python
+
+
+class bcolors:
+    PURPLE = '\033[95m'
+    BLUE = '\033[94m'
+    YELLOW = '\033[93m'
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
