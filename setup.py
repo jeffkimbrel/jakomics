37bde0585d1c3adfe62bf10f29c9bb7ee75239f2
Jeff Kimbrel <jakpot@gmail.com>
2020-04-09 09:10:44 -0700

    Revert "Revert "v0.2.0""
    This reverts commit 5dc919cff28f161e47ba331e4c74c517617b34a3.

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jakomics",
-    version="0.1.0",
+    version="0.2.0",
     author="Jeff Kimbrel",
     author_email="jakpot@gmail.com",
-    description="A small example package",
+    description="Various omics tools",
     long_description=long_description,
     long_description_content_type="text/markdown",
     # packages=setuptools.find_packages(),
