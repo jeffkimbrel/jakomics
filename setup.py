2e6da09c937f1b687404696c8d6dc23f6ef17f86
Jeff Kimbrel <jakpot@gmail.com>
2020-04-09 08:39:56 -0700

    v0.1.0 switched to a package

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jakomics",
+    version="0.1.0",
+    author="Jeff Kimbrel",
+    author_email="jakpot@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # packages=setuptools.find_packages(),
+    # classifiers=[
+    #     "Programming Language :: Python :: 3",
+    #     "License :: OSI Approved :: MIT License",
+    #     "Operating System :: OS Independent",
+    # ],
+    python_requires='>=3.6',
+)
