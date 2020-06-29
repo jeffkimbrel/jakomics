f875a9d6b1f08b73e9b54964709003c6c3799bbe
Jeff Kimbrel <jakpot@gmail.com>
2020-06-29 15:50:30 -0700

    Update file.py

--- jakomics/file.py
+++ jakomics/file.py
@@ -8,6 +8,7 @@ class FILE:
         self.file_path = file_path
         self.name = os.path.basename(self.file_path)
         self.short_name = os.path.splitext(self.name)[0]
+        self.suffix = os.path.splitext(self.name)[1]
         self.id = uuid.uuid4().hex
         self.stats = os.stat(self.file_path)
 
