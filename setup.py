a23b03c6c3c6a15d6d2de3d69c1baa1a72990dfa
Jeff Kimbrel <jakpot@gmail.com>
2020-08-07 15:45:07 -0700

    v0.9.0
    install from github

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jakomics",
-    version="0.8.7",
+    version="0.9.0",
     author="Jeff Kimbrel",
     author_email="jakpot@gmail.com",
     description="Various omics tools",
