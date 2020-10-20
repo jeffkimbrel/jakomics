7f3e7a717ca57b2147398e02154a8b64a9e7384a
Jeff Kimbrel <jakpot@gmail.com>
2020-10-19 21:12:04 -0700

    Update fastq.py

--- jakomics/fastq.py
+++ jakomics/fastq.py
@@ -108,7 +108,7 @@ class FASTQ():
 
         call = 'bbduk.sh in1=' + in1 + ' in2=' + in2 + ' out1=' + \
             self.cf[0] + ' out2=' + self.cf[1] + ' stats=' + \
-            self.processed_sample_name + '_stats.rt.cf.txt t=8 ref=' + db + ' k=31 hdist=1 minlen=50 -Xmx8g'
+            self.processed_sample_name + '_stats.cf.txt t=8 ref=' + db + ' k=31 hdist=1 minlen=50 -Xmx8g'
 
         system_call(call, echo=echo, run=run)
 
