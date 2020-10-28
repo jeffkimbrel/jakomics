639507c902b633118ce8645bc290687d1e7576c1
Jeff Kimbrel <jakpot@gmail.com>
2020-10-28 11:51:16 -0700

    v0.12.10

--- jakomics/bbtools.py
+++ jakomics/bbtools.py
@@ -18,34 +18,36 @@ def extract_stats(lines):
     # stats = {"input": 0, "lowQC": 0, "contamination": 0, "removed": 0, "remain": 0}
     stats = {}
 
-    print(lines)
-
     for line in lines:
         if line.startswith("Input:"):
-            stats["READS_IN"] = line.split()[1]
-            stats["BP_IN"] = line.split()[3]
+            stats["IN"] = {"READS": line.split()[1],  "BP": line.split()[3]}
         elif line.startswith("FTrimmed:"):
-            stats["READS_FTrimmed"] = line.split()[1]
-            stats["BP_FTrimmed"] = line.split()[4]
+            stats["FTrimmed"] = {"READS": line.split()[1],  "BP": line.split()[4]}
+            # stats["READS_FTrimmed"] = line.split()[1]
+            # stats["BP_FTrimmed"] = line.split()[4]
         elif line.startswith("KTrimmed:"):
-            stats["READS_KTrimmed"] = line.split()[1]
-            stats["BP_KTrimmed"] = line.split()[4]
+            stats["KTrimmed"] = {"READS": line.split()[1],  "BP": line.split()[4]}
+            # stats["READS_KTrimmed"] = line.split()[1]
+            # stats["BP_KTrimmed"] = line.split()[4]
         elif line.startswith("Trimmed by overlap::"):
-            stats["READS_Overlap_Trimmed"] = line.split()[3]
-            stats["BP_Overlap_Trimmed"] = line.split()[6]
+            stats["Overlap_Trimmed"] = {"READS": line.split()[3],  "BP": line.split()[6]}
+            # stats["READS_Overlap_Trimmed"] = line.split()[3]
+            # stats["BP_Overlap_Trimmed"] = line.split()[6]
         elif line.startswith("Contaminants:"):
-            stats["READS_CF"] = line.split()[1]
-            stats["BP_CF"] = line.split()[4]
+            stats["CF"] = {"READS": line.split()[1],  "BP": line.split()[4]}
+            # stats["READS_CF"] = line.split()[1]
+            # stats["BP_CF"] = line.split()[4]
         elif line.startswith("QTrimmed:"):
-            stats["READS_QT"] = line.split()[1]
-            stats["BP_QT"] = line.split()[4]
-        elif line.startswith("Low quality discards:"):
-            stats["lowQC"] = line.split()[3]
+            stats["QT"] = {"READS": line.split()[1],  "BP": line.split()[4]}
+            # stats["READS_QT"] = line.split()[1]
+            # stats["BP_QT"] = line.split()[4]
         elif line.startswith("Total Removed:"):
-            stats["READS_REMOVED"] = line.split()[2]
-            stats["BP_REMOVED"] = line.split()[5]
+            stats["REMOVED"] = {"READS": line.split()[2],  "BP": line.split()[5]}
+            # stats["READS_REMOVED"] = line.split()[2]
+            # stats["BP_REMOVED"] = line.split()[5]
         elif line.startswith("Result:"):
-            stats["READS_OUT"] = line.split()[1]
-            stats["BP_OUT"] = line.split()[4]
+            stats["OUT"] = {"READS": line.split()[1],  "BP": line.split()[4]}
+            # stats["READS_OUT"] = line.split()[1]
+            # stats["BP_OUT"] = line.split()[4]
 
     return(stats)
